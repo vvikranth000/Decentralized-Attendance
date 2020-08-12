d875d467267ba970e6fb7f9b81e6912834b5f946
vvikranth000 <42453007+vvikranth000@users.noreply.github.com>
2020-08-12 11:40:42 +0530

    Update Attendance.sol

--- Attendance.sol
+++ Attendance.sol
@@ -1,5 +1,46 @@
 pragma solidity ^0.4.18;
 
+
+library convert{
+    function stringToBytes32(string memory source) internal pure returns (bytes32 result) 
+    {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) 
+        {
+            return 0x0;
+        }
+    
+        assembly 
+        {
+            result := mload(add(source, 32))
+        }
+    }
+    
+    function bytes32ToString(bytes32 x) internal pure returns (string) 
+    {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) 
+        {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) 
+            {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) 
+        {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
+    
+}
+
+
+
 contract Owned 
 {
     address owner;
@@ -15,6 +56,7 @@ contract Owned
        _;
    }
 }
+
 
 contract AttendanceSheet is Owned 
 {
@@ -36,50 +78,18 @@ contract AttendanceSheet is Owned
        uint age
     );
     
-    function stringToBytes32(string memory source) public pure returns (bytes32 result) 
-    {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) 
-        {
-            return 0x0;
-        }
     
-        assembly 
-        {
-            result := mload(add(source, 32))
-        }
-    }
-    
-    function bytes32ToString(bytes32 x) public pure returns (string) 
-    {
-        bytes memory bytesString = new bytes(32);
-        uint charCount = 0;
-        for (uint j = 0; j < 32; j++) 
-        {
-            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-            }
-        }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (j = 0; j < charCount; j++) 
-        {
-            bytesStringTrimmed[j] = bytesString[j];
-        }
-        return string(bytesStringTrimmed);
-    }
     
     function createStudent(uint _studId, uint _age, string _fName, string _lName) onlyOwner public 
     {
         var student = studentList[_studId];
         
         student.age = _age;
-        student.fName = stringToBytes32(_fName);
-        student.lName = stringToBytes32(_lName);
+        student.fName = convert.stringToBytes32(_fName);
+        student.lName = convert.stringToBytes32(_lName);
         student.attendanceValue = 0;
         studIdList.push(_studId) -1;
-        studentCreationEvent(stringToBytes32(_fName), stringToBytes32(_lName), _age);
+        studentCreationEvent(convert.stringToBytes32(_fName), convert.stringToBytes32(_lName), _age);
     }
     
     function incrementAttendance(uint _studId) onlyOwner public {
@@ -91,7 +101,11 @@ contract AttendanceSheet is Owned
     }
     
     function getParticularStudent(uint _studId) public view returns (string, string, uint, uint) {
-        return (bytes32ToString(studentList[_studId].fName), bytes32ToString(studentList[_studId].lName), studentList[_studId].age, studentList[_studId].attendanceValue);
+        return (convert.bytes32ToString(studentList[_studId].fName), convert.bytes32ToString(studentList[_studId].lName), studentList[_studId].age, studentList[_studId].attendanceValue);
+    }
+    
+    function noofattendance(uint _studId) public view returns(uint){
+        return(studentList[_studId].attendanceValue);
     }
 
     function countStudents() view public returns (uint) {
